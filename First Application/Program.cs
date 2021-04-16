69cdbdb2d27b3bd48b302b227c4f77d2b037391c
Marcin Kijak <kijak76@gmail.com>
2021-04-16 17:52:14 +0200

    Add project files.

---
+++ First Application/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace First_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+        }
+    }
+}
