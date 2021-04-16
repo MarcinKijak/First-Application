83552d3780e5635a8900659c90be57bb5033c0fb
Marcin Kijak <kijak76@gmail.com>
2021-04-16 23:10:32 +0200

    pole prostokąta w main

--- First Application/Program.cs
+++ First Application/Program.cs
@@ -6,19 +6,16 @@ namespace FirstApplication
         {
             static void Main(string[] args)
             {
-            //Console.WriteLine("Hello World !");
-            //Console.WriteLine("Press any key to continue...");
-            //Console.ReadKey();
+            //pole prostokąta
+            Console.WriteLine("Pole prostokąta");
+            Console.WriteLine("---------------");
+            Console.WriteLine($" ");
+            Console.WriteLine($" ");
             Console.WriteLine("Podaj wysokość...");
             var a = Console.ReadLine();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
             Console.WriteLine("Podaj podstawę...");
             var b = Console.ReadLine();
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
             var c =  Convert.ToInt32 (a) * Convert.ToInt32(b);
-
             Console.WriteLine($"Pole prostokąta to {c} ");
             Console.ReadKey();
         }
