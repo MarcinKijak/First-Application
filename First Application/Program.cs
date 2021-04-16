b35568aca3bf9cc1d9c5b5b2f1a40102949a8762
Marcin Kijak <kijak76@gmail.com>
2021-04-16 22:58:33 +0200

    First application

--- First Application/Program.cs
+++ First Application/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 
-namespace First_Application
+namespace FirstApplication
 {
     class Program
-    {
-        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Press any key to continue");
+            static void Main(string[] args)
+            {
+            //Console.WriteLine("Hello World !");
+            //Console.WriteLine("Press any key to continue...");
+            //Console.ReadKey();
+            Console.WriteLine("Podaj wysokość...");
+            var a = Console.ReadLine();
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+            Console.WriteLine("Podaj podstawę...");
+            var b = Console.ReadLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            var c =  Convert.ToInt32 (a) * Convert.ToInt32(b);
+
+            Console.WriteLine($"Pole prostokąta to {c} ");
+            Console.ReadKey();
+        }
+
 
         }
-    }
+
 }
